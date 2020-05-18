d37c5606f3bb2fc051e8d1d39e4634e4c14305df
cyzwind <leo1104@hotmail.com.tw>
2020-05-18 10:34:37 +0800

    5/18
    cofig=登入db資料,pass還未設定
    searchpage=搜尋功能

--- model/config.php
+++ model/config.php
@@ -1,6 +1,13 @@
 <?php
     $db_host = 'localhost';
+<<<<<<< HEAD
     $db_user = 'db_final';
     $db_pass = 'a12345678';
     $db_name = 'db_final';
 ?>
+=======
+    $db_user = 'root';
+    $db_pass = '';
+    $db_name = 'db_final';
+?>
+>>>>>>> 5/18
