18079a0a8dd69a5a94f7616d6f834cdafa2b1cf7
starskytw <52898998+starskytw@users.noreply.github.com>
2020-05-21 00:06:13 +0800

    初版navbar
    有些地方還要修改 有可能會把<nav>標籤拿掉 所以在其他css裡面盡量先不要引用nav 麻煩ㄌ

--- index.php
+++ index.php
@@ -1,16 +1,8 @@
 <!DOCTYPE html>
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 
-  <link rel="stylesheet" type = "text/css" href="../src/css/nav.css">
-
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-
+  <?php require("src/head.php")?>
   <title> index </title>
 
 </head>
@@ -18,6 +10,7 @@
 <body>
 
   <?php require("model/navbar.php")?>
+
 
 
 
--- model/navbar.php
+++ model/navbar.php
@@ -1,22 +1,32 @@
-
+<link rel="stylesheet" type = "text/css" href="../src/css/nav.css">
 <div class = "topnav">
-    <nav class = "navbar bg-navbar">
+  <!--有些地方還要修 有可能會把<nav>標籤拿掉 所以在其他css裡面盡量先不要引用nav  by學弟-->
+    <nav class = "navbar bg-navbar ">
       <div class = "container-fluid">
+
           <div class = "col-xl-1">
-            <button type = "button"><a href = "#"></a></button>
+            <a class = "navbar-brand" href="../index.php"> 
+              <!-- 這裡放Logo <img src=""> -->
+            </a>
           </div>
 
           <div class = "col-xl-6 col-xl-offset-2">
             <form>
-                <input type ="text" name = "search" placeholder = "搜尋商品" style ="border:none;width:90%">
+                <input type ="text" name = "search" placeholder = " 搜尋商品" style ="width:90%">
                 <input type ="submit" value = "搜尋">
             </form>
           </div>
 
-          <div class = "col-xl-2 col-xl-offset-1">
-            <a class = "float-right" href = "view/login.php">登入</a>
-            <a class = "float-right" href = "view/register.php">註冊</a>
+          <!-- 跟登入有關的東西寫在這塊底下 -->
+          <div class = "dropdown">
+            <a class = "float-right" href = "#">登入</a>
+            <a class = "h float-right" href = "#">
+              <i class = "fas fa-shopping-cart">
+                <!-- 這裡放購物車的清單 (在nav底下hover好像不能用但先暫訂放這邊) -->
+              </i>
+            </a>
           </div>
+          
       </div>
     </nav>
 </div>
