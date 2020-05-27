26447ee727e51c3e1a25e34b11de2781ecd172fa
starskytw <52898998+starskytw@users.noreply.github.com>
2020-05-27 17:47:12 +0800

    navbar+hover
    倒數第二版

--- model/navbar.php
+++ model/navbar.php
@@ -1,41 +1,64 @@
 <link rel="stylesheet" type = "text/css" href="../src/css/nav.css">
 
+
+<?php session_start() ?>
 <!-- 搜尋欄navbar (會黏在最上層) -->
 <div class = "topnav">
-  <!--有些地方還要修 有可能會把<div container>標籤拿掉 所以在其他css裡面盡量先不要引用container  by學弟-->
+
     <nav class = "navbar bg-navbar ">
-      <div class = "container-fluid">
 
-          <div class = "col-xl-1">
-            <a class = "navbar-brand" href="../index.php"> 
-              <!-- 這裡放Logo <img src=""> -->
-            </a>
-          </div>
-
-          <div class = "col-xl-6 col-xl-offset-2">
-            <form>
-                <input type ="text" name = "search" placeholder = " 搜尋商品" style ="width:90%">
-                <input type ="submit" value = "搜尋">
-            </form>
-          </div>
-
-          <!-- 跟登入有關的東西寫在這塊底下 -->
-          <div class = "dropdown">
-            <a class = "float-right" href = "#">登入</a>
-            <a class = "h float-right" href = "#">
-              <i class = "fas fa-shopping-cart">
-                <!-- 這裡放購物車的清單 (在nav底下hover好像不能用但先暫訂放這邊) -->
-              </i>
-            </a>
-          </div>
+      <a class = "nav-brand" href="../index.php"> 
+        <img src = "../src/images/logo.png" style="width:50%; height:50%">
+      </a>
           
-      </div>
-    </nav>
-</div>
+      <!-- SearchBox -->
+      <form class = "form-inline">
+        <div class = "form-group">
+          <input type ="text" name = "search" placeholder = " 搜尋商品" style ="width:50vw">
+        </div>
 
-<!-- 類別navbar -->
-<div class = "category-nav">
-  <nav class = "navbar bg-category">
-    <!-- 自行增加類別 -->
-  </nav>
+        <div class = "form-group">
+          <input type ="submit" value = "搜尋">
+        </div>            
+      </form>
+          
+
+      <!-- 跟登入有關的東西寫在這塊底下 -->
+      <div class = "nav-right">
+        <div class = "dropdown">
+          <a class = "h float-right" href = "#">
+            <i class = "fas fa-shopping-cart"></i>
+          </a>
+
+          <div class = "dropdown-content dropdown-menu dropdown-menu-right">
+
+            <!-- 這裡放購物車的清單 -->
+            <li>購物車裡面空空的</li>
+
+          </div>
+        </div>
+          
+        <?php
+          if(!isset($_SESSION['isLogin'])){
+        ?>
+
+          <!-- 如果還沒登入則顯示登入按鈕 -->
+          <a class = "float-right" href = "#">登入</a>
+
+        <?php 
+          } 
+          else{
+        ?>
+        
+          <!-- 不然就顯示使用者名稱&登出 -->
+          <a class = "floar-right" href = "#"><?php if(isset($_SESSION['username']) && $_SESSION['username'] != null){ echo "HI, " . $_SESSION['username']; } ?></a>
+          <a class = "float-right" href = "#">登出</a>
+
+        <?php 
+          } 
+        ?>
+      </div>
+          
+    </nav>
+
 </div>
