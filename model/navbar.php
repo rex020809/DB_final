50fd7d805dbc79fc40139ce35b2a0e90e9aeb446
starskytw <52898998+starskytw@users.noreply.github.com>
2020-05-29 15:55:49 +0800

    加上購物車hover了

--- model/navbar.php
+++ model/navbar.php
@@ -7,58 +7,63 @@
 
     <nav class = "navbar bg-navbar ">
 
-      <a class = "nav-brand" href="../index.php"> 
-        <img src = "../src/images/logo.png" style="width:50%; height:50%">
-      </a>
+        <a class = "nav-brand" href="../index.php"> 
+          <img src = "../src/images/logo.png" style="width:50%; height:50%">
+        </a>
           
-      <!-- SearchBox -->
-      <form class = "form-inline">
-        <div class = "form-group">
-          <input type ="text" name = "search" placeholder = " 搜尋商品" style ="width:50vw">
-        </div>
+        <!-- SearchBox -->
+        <form class = "form-inline">
 
-        <div class = "form-group">
-          <input type ="submit" value = "搜尋">
-        </div>            
-      </form>
+          <div class = "form-group">
+            <input type ="text" name = "search" placeholder = " 搜尋商品" style ="width:50vw">
+          </div>
+
+          <div class = "form-group">
+            <input type ="submit" value = "搜尋">
+          </div>
+
+        </form>
           
 
-      <!-- 跟登入有關的東西寫在這塊底下 -->
-      <div class = "nav-right">
-        <div class = "dropdown">
-          <a class = "h float-right" href = "#">
-            <i class = "fas fa-shopping-cart"></i>
-          </a>
+        <!-- 跟登入有關的東西寫在這塊底下 -->
+        <div class = "nav-right">
 
-          <div class = "dropdown-content dropdown-menu dropdown-menu-right">
+          <div class = "dropdown">
 
-            <!-- 這裡放購物車的清單(用cookie) -->
-            <!-- hover有點怪怪的晚點修 -->
-            <li>購物車裡面空空的</li>
+            <a class = "h float-right" href = "#">
+              <i class = "fas fa-shopping-cart"></i>
+            </a>
+
+            <!-- 這裡放被加入購物車內的商品清單(讀取cookie資訊) -->
+            <div class = "dropdown-content dropdown-menu dropdown-menu-right">
+              <li>購物車裡面空空的fdsaffdsafs</li> 
+              <hr>
+            </div>
 
           </div>
-        </div>
           
-        <?php
-          if(!isset($_SESSION['isLogin'])){
-        ?>
+          
+          <?php
+            if(!isset($_SESSION['isLogin'])){
+            ?>
 
-          <!-- 如果還沒登入則顯示登入按鈕 -->
-          <a class = "float-right" href = "#">登入</a>
+            <!-- 如果還沒登入則顯示登入按鈕 -->
+            <a class = "float-right" href = "#">登入</a>
 
-        <?php 
-          } 
-          else{
-        ?>
+            <?php 
+            } 
+            else{
+            ?>
         
-          <!-- 不然就顯示使用者名稱&登出 -->
-          <a class = "floar-right" href = "#"><?php if(isset($_SESSION['name']) && $_SESSION['name'] != null){ echo "HI, " . $_SESSION['name']; } ?></a>
-          <a class = "float-right" href = "#">登出</a>
+            <!-- 不然就顯示使用者名稱&登出 -->
+            <a class = "floar-right" href = "#"><?php if(isset($_SESSION['name']) && $_SESSION['name'] != null){ echo "HI, " . $_SESSION['name']; } ?></a>
+            <a class = "float-right" href = "#">登出</a>
 
-        <?php 
-          } 
-        ?>
-      </div>
+            <?php 
+            } 
+          ?>
+
+        </div>
           
     </nav>
 
