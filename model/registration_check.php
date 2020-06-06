20bb7945fc05ac3a995ee5f83430540f75acbcb9
EricLin <fictionman66@gmail.com>
2020-06-06 23:37:26 +0800

    Update registration_check.php

--- model/registration_check.php
+++ model/registration_check.php
@@ -6,8 +6,7 @@ $c_num = htmlspecialchars($_GET['c_num']);
 $b_date = htmlspecialchars($_GET['b_date']);
 $name = htmlspecialchars($_GET['name']);
 $account = htmlspecialchars($_GET['account']);
-$password = md5(htmlspecialchars($_GET['password']));
-
+$password = htmlspecialchars($_GET['password']);
 
 $email_sql = "SELECT account FROM member WHERE email = '$email';";
 $phone_sql = "SELECT account FROM member WHERE c_num = '$c_num';";
@@ -39,7 +38,6 @@ if(mysqli_num_rows($email_result) === 0 && mysqli_num_rows($phone_result) === 0 
     VALUES ('$account', '$password', '$name', '$c_num', '$email', '$b_date');";
     if (mysqli_query($conn, $sql)) {
       echo "註冊成功";
-      header("Location:../view/plist.php");
     } else {
       echo "Error: " . $sql . "<br>" . $conn->error;
     }
