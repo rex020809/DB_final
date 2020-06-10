264d1fcf43c558f3757bb7863b361afb7eaf75c0
starskytw <52898998+starskytw@users.noreply.github.com>
2020-06-11 03:00:58 +0800

    新增結帳按鈕&歷史訂單

--- model/navbar.php
+++ model/navbar.php
@@ -31,10 +31,31 @@
               <i class = "fas fa-shopping-cart"></i>
             </a>
 
-            <!-- 這裡放被加入購物車內的商品清單(讀取cookie資訊) -->
+            <!-- 這裡放被加入購物車內的商品清單(讀取SESSION資訊) -->
             <div class = "dropdown-content dropdown-menu dropdown-menu-right dropdown-shoppingcart">
-              <!-- <?php // require("../model/shoppingcart.php") shopping_cart() ?>  -->
-              <li>購物車裡面空空的fdsaffdsafs</li>
+
+              <!-- 假設SESSION用 'product' 當購物車的key 可改 反正最後統一就好  -->
+
+              <?php if( !isset($_SESSION['product']) || count($_SESSION['product']) ==0 ) { ?>            
+                <li>購物車裡面空空的fdsaffdsafs</li>
+              <?php 
+              }
+
+              else{   //for(i in $_SESSION['product']){   ?>
+
+
+                      <!--  列出購物清單裡的東西 可以參考plist的寫法 但改成橫行顯示-->
+
+
+              <?php  
+                      //}?>
+
+                <button style="width:inherit" onclick="location.href='../model/checkout.php'">前往結帳頁面</button>
+
+              <?php
+              }
+              ?>
+
             </div>
 
           </div>
@@ -62,8 +83,9 @@
                 <li>帳號： <?php echo $_SESSION['account']; ?></li>
                 <li>信箱： <?php echo $_SESSION['email']; ?></li>
                 <li>手機： <?php echo $_SESSION['c_num']; ?></li>
+                <button class = "dropdown-button" style="width:inherit" onclick="location.href='../model/buy_list.php'"></button>
 
-                <a class = "float-right" href = "../view/reset_password.php">
+                <a href = "../view/reset_password.php">
                   <li> 修改密碼 </li>
                 </a>
               </div>
