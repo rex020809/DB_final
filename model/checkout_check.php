e79575addc4e924e3ae568ba9599030c22264462
EricLin <fictionman66@gmail.com>
2020-06-16 23:30:20 +0800

    後端

--- model/checkout_check.php
+++ model/checkout_check.php
@@ -1,5 +1,25 @@
 <?php
-session_strat();
+session_start();
 require("db_check.php");
 
-$quantity = htmlspecialchars($_GET['quantity']);
+$p_id = 
+$p_num = htmlspecialchars($_GET['quantity']);
+$pay_info = htmlspecialchars($_GET['way']);
+$county = htmlspecialchars($_GET['county']);
+$district = htmlspecialchars($_GET['district']);
+$address = htmlspecialchars($_GET['ship-address']);
+
+$conn = db_check();
+
+$oid_sql = "SELECT MAX(o_id) FROM order_info;";
+$oid_check = mysqli_query($conn, $oid_sql);
+$row = mysqli_fetch_assoc($oid_check);
+
+if(mysqli_num_rows($oid_check)) === 0){
+	$o_id = 1;
+}
+else{
+	$o_id = $row['MAX(o_id)'] + 1;
+}
+
+$o_time = 
