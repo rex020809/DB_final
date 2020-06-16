9c628f86a823df942b6c3642f6876b80623af299
EricLin <fictionman66@gmail.com>
2020-06-16 21:32:54 +0800

    信用卡欄位

--- view/checkout.php
+++ view/checkout.php
@@ -7,6 +7,7 @@
 </head>
 <body>
 	<?php require("../model/navbar.php"); ?>
+  <form>
 	<div class="container-fluid">
 	<div class="row">
 <!-- 商品資訊 -->
@@ -31,23 +32,24 @@
         <div class="col-12"><h1 align="center" valign="center">付款資訊</h1></div>
         <!-- 付款方式選項 -->
 		<div class="col-12">
-			<form id="pay">
-	          <input id="creditcard" type="radio" name="way" value="creditcard">信用卡<br>
-	          <input id="cash" type="radio" name="way" value="cash">貨到付款<br>
-	        </form>
+      <input id="creditcard" type="radio" name="way" value="creditcard">信用卡<br>
+      <input disabled type="text" id="owner">持卡者姓名<br>
+      <input disabled id="cardnum" type="tel" inputmode="numeric" pattern="[0-9\s]{13,19}" autocomplete="cc-number" maxlength="19" placeholder="xxxx xxxx xxxx xxxx">卡號<br>
+      <input disabled id="date" type="tel" pattern="[0-1][0-12]/[0-9]{2}" placeholder="MM/YY">到期日<br>
+      <input disabled id="cvv" type="tel" inputmode="numeric" pattern="[0-9]{3}" placeholder="xxx">CVV<br>
+	   <input id="cash" type="radio" name="way" value="cash">貨到付款<br>
+	     
 		</div>
-      </div>
+    </div>
 <!-- 寄送資訊 -->
       <div class="row">
         <div class="col-12"><h1 align="center" valign="center">寄送資訊</h1></div>
 		<div class="col-12">
-			<form id="address" name="address" method="POST" action="#">
-	          <label for="frmAddressS">地址</label>
-	          <div id="twzipcode">
-	            <input type="text" name="ship-address" required id="frmAddressS" placeholder="街／路、樓層" autocomplete="shipping street-address">
-	          </div>
+      <label for="frmAddressS">地址</label>
+      <div id="twzipcode">
+        <input type="text" name="ship-address" required id="frmAddressS" placeholder="街／路、樓層" autocomplete="shipping street-address">
+      </div>
 		</div>
-        </form>
       </div>
 <!-- 訂單資訊 -->
       <div class="row">
@@ -57,11 +59,12 @@
           <div class="row">5555</div>
           <div class="row">1000</div>
           <div class="row">6555</div>
-          <div class="row"><button type="submit">下訂單</button></div>
+          <div class=row><input type="submit" value="下訂單"></div>
         </div>
       </div>
-	</div>
-
+  </div>
+</div>
+</form>
 
 	<?php require('footer.php');?>
 </body>
@@ -85,7 +88,21 @@ $('#price').on( 'click','.quantity',function(){
 });
 
 $('#creditcard').on('click',function(){
+  $("#owner").removeAttr("disabled");
+  $("#cardnum").removeAttr("disabled");
+  $("#date").removeAttr("disabled");
+  $("#cvv").removeAttr("disabled");  
+})
 
+$('#cash').on('click',function(){
+  var owner = document.getElementById("owner");
+  var cardnum = document.getElementById("cardnum");
+  var date = document.getElementById("date");
+  var cvv = document.getElementById("cvv");
+  owner.setAttribute("disabled","");
+  cardnum.setAttribute("disabled","");
+  date.setAttribute("disabled","");
+  cvv.setAttribute("disabled","");
 })
 
 </script>
