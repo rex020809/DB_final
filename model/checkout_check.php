6978d0e549cfd3fcda36b9741ebce28b9b1279bf
rex020809 <rex020809.tem06@nctu.edu.tw>
2020-06-16 23:41:14 +0800

    結帳頁面完成剩後端

--- model/checkout_check.php
+++ model/checkout_check.php
@@ -2,7 +2,8 @@
 session_start();
 require("db_check.php");
 
-$p_id = 
+print_r($_GET['id']);
+//$p_id =
 $p_num = htmlspecialchars($_GET['quantity']);
 $pay_info = htmlspecialchars($_GET['way']);
 $county = htmlspecialchars($_GET['county']);
@@ -15,11 +16,11 @@ $oid_sql = "SELECT MAX(o_id) FROM order_info;";
 $oid_check = mysqli_query($conn, $oid_sql);
 $row = mysqli_fetch_assoc($oid_check);
 
-if(mysqli_num_rows($oid_check)) === 0){
+if(mysqli_num_rows($oid_check) === 0){
 	$o_id = 1;
 }
 else{
 	$o_id = $row['MAX(o_id)'] + 1;
 }
 
-$o_time = 
+//$o_time =
