2af33b6344aeeaa9c217157bea1286a1f88995f9
starskytw <52898998+starskytw@users.noreply.github.com>
2020-06-16 22:50:02 +0800

    修正過後的歷史訂單(未排版)

---
+++ view/buy_list.php
@@ -0,0 +1,119 @@
+<?php $is_view = TRUE; ?>
+
+<html>
+    <head>
+
+        <?php require('../src/head.php');?>
+        <?php require('../model/navbar.php');?>
+    </head>
+
+    <body>
+
+        <?php
+
+        require("../model/db_check.php");
+
+        if(isset($_SESSION["account"])&& $_SESSION["account"] != "")
+        {
+           $account = $_SESSION["account"];
+        }
+        else
+        {
+            header("Location: ../view/login.php");
+        }
+
+        $conn       = db_check();
+        $sql        = "SELECT * FROM order_info WHERE buyer = '$account' " ;
+        $result     = mysqli_query($conn, $sql);
+        $result_num = mysqli_num_rows($result);
+
+
+        // 如果商城內沒有這個人的購買紀錄 就印出官方回答
+        if($result_num == 0)
+        {
+            echo "之前還沒有買過東西呢! 現在去商城選選吧!";
+        }
+        // 不然就把歷史紀錄全部列出來(using listview)
+        else
+        {
+        ?>
+            <div class = "transaction_list">
+                <div class = "title">
+                    <p>訂單編號 商品編號 商品名稱 款式 數量 目前進度 交易時間</p>
+                </div>
+
+            <?php
+
+            // 取得未完成的訂單資料並逐行列出
+            for($i = 0; $i < $result_num; $i++)
+            {
+                $row = mysqli_fetch_assoc($result);
+
+                if($row['o_prog'] == "未完成")
+                {
+                    $o_id       = $row['o_id'];
+                    $p_id       = $row['p_id'];
+                    $p_style    = $row['p_style'];
+                    $p_num      = $row['p_num'];
+                    $o_prog     = $row['o_prog'];
+                    $pay_info   = $row['pay_info'];
+                    $o_time     = $row['o_time'];
+
+
+                    $sql_pname  = "SELECT p_name FROM product WHERE p_id = $p_id";
+                    $result_pname = mysqli_query($conn, $sql_pname);
+                    $p_name = mysqli_fetch_assoc($result_pname)['p_name'];
+            ?>
+                    <hr>
+                    <div class = "column">
+                        <p><?php echo $o_id." ".$p_id." ".$p_name." ".$p_style." ".$p_num." ".$o_prog." ".$o_time; ?> </p>
+                    </div>
+
+            <?php
+                }
+            }
+            ?>
+
+            </div>
+
+            <?php
+
+            // 取得已完成的訂單資料並逐行列出
+            $result     = mysqli_query($conn, $sql);
+            for($i = 0; $i < $result_num; $i++)
+            {
+                $row = mysqli_fetch_assoc($result);
+
+                if($row['o_prog'] != "未完成")
+                {
+                    $o_id       = $row['o_id'];
+                    $p_id       = $row['p_id'];
+                    $p_style    = $row['p_style'];
+                    $p_num      = $row['p_num'];
+                    $o_prog     = $row['o_prog'];
+                    $pay_info   = $row['pay_info'];
+                    $o_time     = $row['o_time'];
+
+
+                    $sql_pname  = "SELECT p_name FROM product WHERE p_id = $p_id";
+                    $result_pname = mysqli_query($conn, $sql_pname);
+                    $p_name = mysqli_fetch_assoc($result_pname)['p_name'];
+            ?>
+                    <hr>
+                    <div class = "column">
+                        <p><?php echo $o_id." ".$p_id." ".$p_name." ".$p_style." ".$p_num." ".$o_prog." ".$o_time; ?> </p>
+                    </div>
+
+            <?php
+                }
+            }
+            ?>
+
+            </div>
+        <?php
+        }
+        ?>
+                    
+
+    </body>
+</html>
